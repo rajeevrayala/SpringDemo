5ae0d09ba7162552f77f90da2cbd88248c049d2d
rajee <rajee@DESKTOP-TB3MB7B>
2017-08-09 15:20:06 -0500

    commit-new branch

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -77,7 +77,9 @@ System.out.println("Welcome to GIT");
 System.out.println("hgcdskhdf");
 System.out.println("hi");
 System.out.println("no");
-		/*Collection<Hello> list = (List<Hello>) map.values();
+System.out.println("new - branch");
+
+/*Collection<Hello> list = (List<Hello>) map.values();
 		ArrayList<Hello> a = new ArrayList<Hello>();
 		a.addAll(list);
 		System.out.println(a);*/
